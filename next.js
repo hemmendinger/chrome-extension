52adfd69bdf71cb0b7a043f2cd4af77169816867
Hemmendinger <github@hemmendinger.net>
2013-10-31 17:15:46 -0400

    starting next.js

---
+++ next.js
@@ -0,0 +1,13 @@
+function nextPage() {
+    var AMAZON_DOMAIN = "www.amazon.com";
+
+    var nextPage = function () {
+	document.getElementBy
+    };
+
+};
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  nextPage();
+});
