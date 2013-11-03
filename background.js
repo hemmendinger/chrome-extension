5637ea85bd5e9f86c8a52eeb9c216a7c1baa8e20
Hemmendinger <github@hemmendinger.net>
2013-11-03 13:13:18 -0500

    changed response message to be more descriptive

--- background.js
+++ background.js
@@ -5,7 +5,7 @@ chrome.browserAction.onClicked.addListener(function (){
 
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {backgroundClick: true}, function(response) {
-            console.log(response.farewell);
+            console.log(response.result);
         });
     });
 });
--- content.js
+++ content.js
@@ -12,5 +12,5 @@ chrome.runtime.onMessage.addListener(
             "from a content script:" + sender.tab.url :
             "from the extension");
         if (request.backgroundClick == true)
-            sendResponse({farewell: "goodbye"});
+            sendResponse({result: "content.js processed backgroundClick == true"});
     });
