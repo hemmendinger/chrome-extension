28286cf91b5ab7cd6309d7a3938acc6859ecc60f
Hemmendinger <github@hemmendinger.net>
2013-11-03 12:57:34 -0500

    adding sendMessage to content.js from background.js

--- background.js
+++ background.js
@@ -1,8 +1,11 @@
 console.log("background.js loaded");
 
 chrome.browserAction.onClicked.addListener(function (){
-        console.log("browser action clicked");
+    console.log("browser action clicked");
 
-
-    }
-);
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+            console.log(response.farewell);
+        });
+    });
+});
