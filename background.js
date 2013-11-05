27826ae9b69bf55b63ea906b0c6040e8569aa3fd
Hemmendinger <github@hemmendinger.net>
2013-11-04 21:40:27 -0500

    menu item handler added(background.js)

--- background.js
+++ background.js
@@ -1,5 +1,16 @@
 console.log("background.js loaded");
 
+
+function onClickedMenuItemHandler(info, tab) {
+    if (info.menuItemId == "margin25pct") {
+        sendMessageToContent(menuItemId, tab);
+    };
+};
+
+function sendMessageToContent(msg, tab) {
+
+}
+
 chrome.browserAction.onClicked.addListener(function (){
     console.log("browser action clicked");
 
